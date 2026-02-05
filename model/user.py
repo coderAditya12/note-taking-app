6cf0de5baff85d4a07fa2edcef6fa504ca14864f
aditya <realadityagupta2@gmail.com>
2026-02-05 21:46:02 +0530

    notes model built

--- model/user.py
+++ model/user.py
@@ -2,7 +2,9 @@ from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column
 from sqlalchemy import String
 from bcrypt import gensalt,hashpw,checkpw
 from  uuid import uuid4,UUID
-class User(DeclarativeBase):
+class Base(DeclarativeBase):
+    pass
+class User(Base):
     __tablename__="user"
     id:Mapped[UUID] = mapped_column(primary_key=True,default=uuid4)
     name:Mapped[str]=mapped_column(String(50))
@@ -10,9 +12,9 @@ class User(DeclarativeBase):
     password:Mapped[str]=mapped_column(String(255),nullable=False)
     
     def set_password(self,password:str):
-        passowrd_in_bytes = password.encode('utf-8')
-        print("password in bytes")
-        self.password= hashpw(passowrd_in_bytes,gensalt()).decode()
+        passoword_in_bytes = password.encode('utf-8')
+        print("password in bytes",passoword_in_bytes)
+        self.password= hashpw(passoword_in_bytes,gensalt()).decode()
         print("password decoding", self.password)
     def check_password(self,password:str)->bool:
         return checkpw(password.encode(),self.password.encode('utf-8'))
